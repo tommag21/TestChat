94bad3a830f022839909a21dce4bdbd8d6e8c0b6
Administrator <Administrator@DESKTOP-NFE9Q08>
2018-10-10 12:33:06 +0200

    Add project files.

---
+++ TestChat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
