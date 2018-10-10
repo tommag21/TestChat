84f3986a9ee7e87c183b04255e34924d72029ad1
Administrator <Administrator@DESKTOP-NFE9Q08>
2018-10-10 13:03:18 +0200

    robe

--- TestChat/Program.cs
+++ TestChat/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SendBird;
+using System;
+using System.Collections.Generic;
 
 namespace TestChat
 {
@@ -6,7 +8,30 @@ namespace TestChat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SendBirdClient.Init("52C4347B-C9B0-4E62-81D6-96EB40517902");
+
+            //SendBirdClient.Connect("tom", (User user, SendBirdException e) =>
+            //{
+            //    if (e != null)
+            //    {
+            //        // Error
+            //        return;
+            //    }
+            //});
+
+            OpenChannel.GetChannel("sendbird_open_channel_43739_172b1997dcc4c9bbfc49bc0533303c8017bb2d70", (OpenChannel openChannel, SendBirdException e) => {
+                if (e != null)
+                {
+                    // Error!
+                    return;
+                }
+
+                Console.WriteLine("canale aperto");
+                // Successfully fetched the channel.
+                // Do something with openChannel.
+            });
+
+            Console.Read();
         }
     }
 }
